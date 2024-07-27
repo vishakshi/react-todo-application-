ada5982df08e5871e44371da2d9c8538ef6e9b22
Vishakshi Gupta <162357622+vishakshi@users.noreply.github.com>
2024-07-27 16:33:51 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,62 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import manifest from "./manifest";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+      registerType: "autoUpdate",
+      workbox: {
+        // Use runtime caching for dynamic imports
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) =>
+              request.destination === "script" ||
+              request.destination === "style" ||
+              request.destination === "font" ||
+              request.destination === "worker",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "dynamic-resources",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.mode === "navigate",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "documents",
+              networkTimeoutSeconds: 10,
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+            },
+          },
+        ],
+      },
+      includeAssets: ["**/*"],
+      manifest,
+    }),
+  ],
+});
